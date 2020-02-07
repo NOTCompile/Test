f7bd5cdc7f47f2a94e8f7522cd98f0c69f599ee6
test <aldhair.mamani@tecsup.edu.pe>
2020-02-07 00:26:55 -0500

    asd

--- resources/views/joshua.blade.php
+++ resources/views/joshua.blade.php
@@ -22,6 +22,23 @@
       </div>
      </div>
 
+     <div class="container">
+<input type="time" name="" value="00:00">
+
+<input type="date" name="" value="00:00">
+     </div>
+     <form>
+      <div>
+        <label for="appt-time">Choose an appointment time (opening hours 12:00 to 18:00): </label>
+        <input id="appt-time" type="time" name="appt-time"
+               min="12:00" max="18:00" required
+               pattern="[0-9]{2}:[0-9]{2}">
+        <span class="validity"></span>
+      </div>
+      <div>
+          <input type="submit" value="Submit form">
+      </div>
+    </form>
 
       <script type="text/javascript" style="">
         $(document).ready(function() {
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -54,13 +54,17 @@
     </div>
   </div>
 
- {{--<script src="{{ route('js\jquery-3.4.1.js') }}"></script>--}}
- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSF1Bw8HfCJo=" crossorigin="anonymous"></script>
+ 
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
   <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  
+  <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/jquery-3.4.1.js') }}"></script>
+  
 </body>
 
 </html>
